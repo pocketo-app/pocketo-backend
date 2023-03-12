07509c03371ee3365f008adf63ba3e097e897565
Vu Tong <tonghoangvu@outlook.com>
2023-03-12 22:42:50 +0700

    code: init project

---
+++ src/main/java/dev/pocketo/pocketobackend/PocketoBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.pocketo.pocketobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pocketo/pocketobackend/PocketoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pocketo.pocketobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
