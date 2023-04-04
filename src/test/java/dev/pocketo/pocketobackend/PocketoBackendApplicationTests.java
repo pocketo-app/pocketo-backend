2c4cd93a03fd46a41746960c62783eaf6a8a1397
Vu Tong <tonghoangvu@outlook.com>
2023-04-04 14:18:54 +0700

    chore: remove contextLoads() integration test
    Because it loads the whole Spring Boot app, which cannot run without Docker Compose

--- src/test/java/dev/pocketo/pocketobackend/PocketoBackendApplicationTests.java
+++ src/test/java/dev/pocketo/pocketobackend/PocketoBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.pocketo.pocketobackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PocketoBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
